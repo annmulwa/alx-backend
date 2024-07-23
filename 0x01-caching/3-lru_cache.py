565dab64dd18375a1e144b372dd83019013f7b77
annmulwa <annmulwa2000@gmail.com>
2024-07-23 17:33:34 +0300

    caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Class LRUCache that inherits from BaseCaching and is a caching system.
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Inherits from BaseCaching and is a caching system.
+    """
+    def __init__(self):
+        """
+        Initializes cache
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Assign to the dictionary self.cache_data the item value for the key.
+        """
+        if key is None and item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        self.cache_data.get(key, None)
