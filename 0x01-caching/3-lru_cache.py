57c3ea5b7b4875306dba0da3d9994fabeec17abe
annmulwa <annmulwa2000@gmail.com>
2024-07-24 18:18:45 +0300

    caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,41 +1,31 @@
-#!/usr/bin/env python3
-"""
-Class LRUCache that inherits from BaseCaching and is a caching system.
-"""
-from collections import OrderedDict
-from base_caching import BaseCaching
+#!/usr/bin/python3
+"""Create LRUCache class that inherits from BaseCaching"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    Inherits from BaseCaching and is a caching system.
-    """
+    """ Define LRUCache """
+
     def __init__(self):
-        """
-        Initializes cache
-        """
+        """ Initialize LRUCache """
+        self.queue = []
         super().__init__()
-        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """
-        Assign to the dictionary self.cache_data the item value for the key.
-        """
-        if key is None and item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
+        """ Assign the item to the dictionary """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
             self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
-        else:
-            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
 
     def get(self, key):
-        """
-        Return the value in self.cache_data linked to key.
-        """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-        self.cache_data.get(key, None)
+        """ Return the value associated with the given key """
+        if self.cache_data.get(key):
+            self.queue.remove(key)
+            self.queue.append(key)
+        return self.cache_data.get(key)
