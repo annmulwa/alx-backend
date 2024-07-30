91dcd9953229992db3a002491836f1d50e33b702
annmulwa <annmulwa2000@gmail.com>
2024-07-30 13:33:23 +0300

    flask app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,12 +21,20 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route('/')
 def get_index() -> str:
     """
     Index page
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
